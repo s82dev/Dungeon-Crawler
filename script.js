a555b5d2483277ac9d10d75660b4251027c48e9a
s82dev <lolcpcreal@gmail.com>
2026-05-26 18:13:42 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("editor").onclick = () => {
+
+    alert("Editor kommt später");
+
+};
+
+document.getElementById("play").onclick = () => {
+
+    alert("Spiel startet");
+
+};
+
+document.getElementById("discord").onclick = () => {
+
+    window.open("https://discord.com");
+
+};
